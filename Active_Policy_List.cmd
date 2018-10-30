50649ebbab070e3dc3dc3793efd12b6cbff3be38
jugalgehani <36434668+jugalgehani@users.noreply.github.com>
2018-10-30 17:14:35 +0530

    Add files via upload

---
+++ Active_Policy_List.cmd
@@ -0,0 +1,11 @@
+del /F "C:\Output\Active_Policy_List.csv"
+
+cd "C:\Program Files\Microsoft SQL Server\MSSQL11.SQLEXPRESS\MSSQL\Binn"
+
+SQLCMD -S "IN-AIR-GPADM1\SQLEXPRESS" -U "sa" -P "Jan@2016" -W -Q "Select POLICY_ID,POLICY_NAME,CUSTOMER_ID,FUND_VALUE from JUGEHANI.dbo.Policy where ACTIVE='Y';" > C:\Output\Active_Policy_List.csv -h-1 -s,
+
+timeout /t 10 /nobreak > NUL
+
+cd C:\Weblogic_12c
+
+powershell ./Active_Policy_List.ps1
